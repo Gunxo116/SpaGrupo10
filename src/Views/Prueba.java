2c462fc50dd2255ec9b1922216bf8344ca795702
Yamil Barbeito <145397981+Gunxo116@users.noreply.github.com>
2025-10-20 19:03:59 -0300

    Update Prueba.java

--- src/Views/Prueba.java
+++ src/Views/Prueba.java
@@ -20,7 +20,7 @@ public class Prueba {
         
         System.out.println("=== Prueba de Conexion a Base de Datos ===");
         Connection conn = ConexionDB.getConexion();
-        
+        System.out.println(" ");
         if (conn != null) {
             System.out.println("Conexion: " + (ConexionDB.isConectado() ? "EXITOSA" : "FALLIDA"));
             
@@ -33,7 +33,7 @@ public class Prueba {
         System.out.println("Integrantes del Grupo 10:");
         System.out.println("- Barbeito Yamil");
         System.out.println("- Redon Estaban");
-        System.out.println("- Atahualpa Blaco");
+        System.out.println("- Atahualpa Blanco");
     }
         
 }
